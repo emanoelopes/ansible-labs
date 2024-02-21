b6a2b6a69c917a796a7bc9c1e96889c74c7f14e5
Emanoel Lopes <emanoelopes@gmail.com>
2024-02-21 10:24:02 -0300

    Applying best practices structure

--- playbooks/roles/faixas/files/bginfo/bginfo.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-C:\Aplicativos\bginfo\Bginfo.exe /accepteula C:\Aplicativos\bginfo\usuario_ip.bgi /timer:0 /silent
---
+++ roles/smd/files/bginfo/bginfo.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Aplicativos\bginfo\Bginfo.exe /accepteula C:\Aplicativos\bginfo\usuario_ip.bgi /timer:0 /silent
